f60be92abd7308e0b25ca2da45b216a8a2c8d1e9
Anfer101 <andreykutuzov2010@gmail.com>
2025-04-21 18:29:16 +0300

    Add files via upload

--- Курсовая.py
+++ Курсовая.py
@@ -19,6 +19,9 @@ food = random.randint(0,1)
 game_close = False
 font_style = pygame.font.SysFont("bahnschrift", 25)
 game_over = 0
+x_circle = 50
+y_circle = 400
+score = 3
 # радиус круга
 r = 40
 
@@ -37,11 +40,12 @@ def message(msg, color):
 def message1(msg,color):
    mesg = font_style.render(msg, True, color)
    screen.blit(mesg, [100,400])
-
-
+def draw_score(screen, score):
+   score_surface = font_style.render(f"Призы: {score}", True, (255, 255, 255))
+   screen.blit(score_surface, (10, 10))
 while game_close != True:
     while game_over == 1:
-           screen.fill(blue)
+           screen.fill(neutral)
            message("Вы проиграли! Нажмите Q для выхода", red)
            message1("или C для повторной игры", red)
            pygame.display.update()
@@ -59,21 +63,36 @@ while game_close != True:
     if food == 0:
         ne = green
         pygame.display.update()
+        if x == 700:
+            game_over = 1
+        elif x ==100:
+           x = 400
+           y = 400
+           ne = (125,125,125)
+           en = (125,125,125)
+           food = random.randint(0,1)
+           screen.fill((boom))
+           pygame.draw.circle(screen, red, (x, y), r)
+           pygame.display.update()
+           pygame.time.delay(65)
+         
+         
     if food ==1:
         en = green
-        pygame.display.update()    
-    if x ==100:
-        x = 400
-        y = 400
-        ne = (125,125,125)
-        en = (125,125,125)
-        food = random.randint(0,1)
-        screen.fill((boom))
-        pygame.draw.circle(screen, red, (x, y), r)
         pygame.display.update()
-        pygame.time.delay(65)
-    if x == 700:
-        game_over = 1
+        if x == 100:
+            game_over = 1
+        elif x ==700:
+           x = 400
+           y = 400
+           ne = (125,125,125)
+           en = (125,125,125)
+           food = random.randint(0,1)
+           screen.fill((boom))
+           pygame.draw.circle(screen, red, (x, y), r)
+           pygame.display.update()
+           pygame.time.delay(65)
+         
     screen.fill((neutral))
     pygame.draw.rect(screen, ne, [50,350, 90,100])
     pygame.draw.rect(screen, en, [660,350, 90,100])
